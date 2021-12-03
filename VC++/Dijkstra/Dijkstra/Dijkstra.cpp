8042cd1560b68ec33966360545c3d9f1a8afea2b
ckdduf <ckdduf138@yuhan.ac.kr>
2021-12-03 19:54:32 +0900

    다익스트리

--- VC++/Dijkstra/Dijkstra/Dijkstra.cpp
+++ VC++/Dijkstra/Dijkstra/Dijkstra.cpp
@@ -146,6 +146,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
+
+            TextOut(hdc, 500, 300, L"다익스트라 구현",8);
+
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
             EndPaint(hWnd, &ps);
         }
